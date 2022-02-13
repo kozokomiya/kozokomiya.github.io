b19f9c7f2bff6447c112d2ac274c157afa79c042
Kozo Komiya <kozo.komiya@gmail.com>
2022-02-13 14:32:48 +0900

    add omni

---
+++ vuetest/sfc/vc-main-1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="vc-main-1">
+    <h1>Hello</h1>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      message: 'こんにちは'
+    }
+  }
+}
+</script>
+
+<style>
+.vc-main-1 h1{
+    font-size: 3.5rem;
+}
+.vc-main-1 p{
+    font-weight: bold;
+}
+</style>
